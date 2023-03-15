4a297ee96e8eac346bf73a5946e5bfde4d6ada68
BetterCallAle <dap22091998@gmail.com>
2023-03-15 14:43:06 +0100

    add code to main

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -2,6 +2,28 @@ package org.lessons.java;
 
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            Dog dog = new Dog("male", "Link", 1, "Beagle");
+            dog.eat();
+            dog.sound();
+            dog.sleep();
+            System.out.println("-----------");
+            Sparrow sparrow = new Sparrow("female", "Zelda", 2, "black");
+            sparrow.eat();
+            sparrow.sound();
+            sparrow.sleep();
+            System.out.println("-----------");
+            Eagle eagle = new Eagle("female", "Ellie", 4, "royal");
+            eagle.eat();
+            eagle.sound();
+            eagle.sleep();
+            System.out.println("-----------");
+            Dolphin dolphin = new Dolphin("male", "Joel", 5);
+            dolphin.eat();
+            dolphin.sound();
+            dolphin.sleep();
+        } catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
