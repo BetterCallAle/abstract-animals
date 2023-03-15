f56e9db913977f9a072ee606b1dce4a6026c7d50
BetterCallAle <dap22091998@gmail.com>
2023-03-15 14:15:44 +0100

    Animal abstract class created

--- src/org/lessons/java/Animal.java
+++ src/org/lessons/java/Animal.java
@@ -1,4 +1,76 @@
 package org.lessons.java;
 
-public class Animal {
+public abstract class Animal {
+    private String gender;
+    private String name;
+    private int age;
+
+    //CONSTRUCTOR
+    public Animal(String gender, String name, int age){
+        checkString(gender, "genere");
+        checkString(name, "nome");
+        checkAge(age);
+        this.gender = gender;
+        this.name = name;
+        this.age = age;
+    }
+
+    //GETTERS
+    public String getGender() {
+        return gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    //SETTERS
+    public void setGender(String gender) {
+        checkString(gender, "genere");
+        this.gender = gender;
+    }
+
+    public void setName(String name) {
+        checkString(name, "nome");
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        checkAge(age);
+        this.age = age;
+    }
+
+    //METHODS
+
+
+    @Override
+    public String toString() {
+        return  "gender='" + gender + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age;
+    }
+
+    private void checkString(String s, String message){
+        if(s.trim().isEmpty() ){
+            throw new IllegalArgumentException("Il " + message + " deve essere inserito");
+        }
+    }
+
+    private void checkAge(int age){
+        if(age < 0){
+            throw new IllegalArgumentException("L'età non può essere minore di zero");
+        }
+    }
+
+    public void sleep(){
+        System.out.println("Zzzzzz");
+    }
+
+    public abstract void sound();
+
+    public abstract void eat();
 }
